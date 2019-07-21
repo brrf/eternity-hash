440942db1e58f7492d6a5f64a8d0e8d8e23a8e32
Moshe Praver <moshepraver@gmail.com>
2019-07-21 16:06:12 -0500

    created register form

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const User = require('../schemas/users')
 
 module.exports = function (app) {
-	app.route('/authentication')
+	app.route('/authentication/login')
 		.get( (req, res) => {
-			res.sendFile(path.join(__dirname, '../public/authenticate.html'))
+			res.sendFile(path.join(__dirname, '../public/login.html'))
+		})
+		.post(async (req, res) => {
+			User.
+			res.send('working')
+		})
+	app.route('/authentication/register')
+		.get( (req, res) => {
+			res.sendFile(path.join(__dirname, '../public/register.html'))
 		})
 }
