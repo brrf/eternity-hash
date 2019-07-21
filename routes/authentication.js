d9f87b5265059b61f74321014e0cdecb2d256f71
Moshe Praver <moshepraver@gmail.com>
2019-07-21 16:39:40 -0500

    server side validation of register form

--- routes/authentication.js
+++ routes/authentication.js
@@ -7,11 +7,38 @@ module.exports = function (app) {
 			res.sendFile(path.join(__dirname, '../public/login.html'))
 		})
 		.post(async (req, res) => {
-			User.
 			res.send('working')
 		})
 	app.route('/authentication/register')
 		.get( (req, res) => {
 			res.sendFile(path.join(__dirname, '../public/register.html'))
+		})
+		.post( async (req, res) => {
+			let errors = [];
+			const {username, password, password2, fname, lname, email} = req.body;
+			if (!username || !password || !password2 || !fname || !lname || !email) {
+				errors.push('please fill out all items');
+			};
+			if (password !== password2) {
+				errors.push('passwords do not match')
+			};
+
+			let user = await User.findOne({email});
+			if (user) {
+				errors.push('Email is already registered. Try logging in!')
+			} 
+			if (errors.length > 0) {
+				res.json(errors)
+			} else {
+				await User.create({
+					username,
+					password,
+					fname,
+					lname,
+					email
+				})
+				res.send('registered!')
+			}
+			
 		})
 }
