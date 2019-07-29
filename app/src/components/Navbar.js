4d95d55d7367c72734d1b0b7fff588776562c8e9
Moshe Praver <moshepraver@gmail.com>
2019-07-29 12:53:35 -0500

    created dropdown menu with login/logout option

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -4,12 +4,22 @@ import {Link} from 'react-router-dom'
 
 function Navbar (props) {
 	let fname = props.fname ? (props.fname.charAt(0).toUpperCase() + props.fname.slice(1)) : null;
+	let loginStatus = props.fname ? true : false; 
+	let authenticationLink = loginStatus ? {link: './authentication/logout', title: 'Logout'} : {link: './authentication/login', title: 'Login'};
 	return (
 		<div className='navbar'>
           <h1>Eternity Hash</h1>
           <div className='navbar-right'>
-              <Link className='navbar-link' to="./authentication/login">{fname ? `${fname}'s Account` : 'My Account'}</Link>
-              <Link className='navbar-link' to="./authentication/login"> Browse Collection</Link>
+              <div className='navbar-item'>
+              	<div className='dropdown-button'>{fname ? `${fname}'s Account` : 'My Account'}
+              	<div className='dropdown-items'>
+              		<Link to='./comingsoon' className='dropdown-item'>Cart</Link>
+              		<Link to='./comingsoon' className='dropdown-item'>My Hashes</Link>
+              		<Link to={authenticationLink.link} className='dropdown-item'>{authenticationLink.title}</Link>
+              	</div>
+              	</div>
+              </div>
+              <Link className='navbar-item' to="./comingsoon"> Browse Collection</Link>
           </div>
         </div>
 	)
